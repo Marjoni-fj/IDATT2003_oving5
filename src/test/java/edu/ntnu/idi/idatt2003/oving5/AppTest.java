015dc4cf7595211c31dd7b09b54a0d41a7ea06c8
Fredrik Marjoni <marjonifredrik@gmail.com>
2026-02-18 13:30:22 +0100

    init: intialize maven project and prepare for oving5

---
+++ src/test/java/edu/ntnu/idi/idatt2003/oving5/AppTest.java
@@ -0,0 +1,13 @@
+package edu.ntnu.idi.idatt2003.oving5;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class AppTest {
+
+  @Test
+  void testApp() {
+    // simple test
+    assertTrue(true);
+  }
+}
