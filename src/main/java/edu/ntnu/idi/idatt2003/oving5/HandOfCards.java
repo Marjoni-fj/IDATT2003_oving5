17f96bdfb6a08050bcfeae81cdb0ef8ea10511cb
Fredrik Marjoni <marjonifredrik@gmail.com>
2026-02-19 16:18:52 +0100

    feat: add methods to check value of a hand of cards

--- src/main/java/edu/ntnu/idi/idatt2003/oving5/HandOfCards.java
+++ src/main/java/edu/ntnu/idi/idatt2003/oving5/HandOfCards.java
@@ -1,13 +1,48 @@
 package edu.ntnu.idi.idatt2003.oving5;
 
 public class HandOfCards {
-  private DeckOfCards deckOfCards;
-  private PlayingCard[] handOfCards;
+  private final PlayingCard[] handOfCards;
 
+    public HandOfCards(PlayingCard[] cards) {
+        this.handOfCards = cards;
+    }
 
-  public HandOfCards() {
-    this.deckOfCards = new DeckOfCards();
-    this.handOfCards = deckOfCards.dealHand(5);
-  }
+    public PlayingCard[] getCards() {
+        return handOfCards;
+    }
+
+    public int sumOfCardValues() {
+        int sum = 0;
+        for (PlayingCard card : handOfCards) {
+            sum += card.getFace();
+        }
+        return sum;
+    }
+
+    public boolean hasPair() {
+      for (int i = 0; i < handOfCards.length; i++) {
+          for (int j = i + 1; j < handOfCards.length; j++) {
+              if (handOfCards[i].getFace() == handOfCards[j].getFace()) {
+                  return true;
+              }
+          }
+      }
+      return false;
+    }
+
+    public boolean hasThreeOfAKind() {
+      for (PlayingCard card : handOfCards) {
+          int count = 0;
+          for (PlayingCard otherCard : handOfCards) {
+              if (card.getFace() == otherCard.getFace()) {
+                  count++;
+              }
+          }
+          if (count == 3) {
+              return true;
+          }
+      }
+      return false;
+    }
 
 }
