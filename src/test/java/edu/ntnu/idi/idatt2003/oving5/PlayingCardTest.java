8379178e1436f30a3561d22ffae20a1374b73f20
Fredrik Marjoni <marjonifredrik@gmail.com>
2026-02-19 13:20:37 +0100

    feat[PlayingCardTest.java]: add more tests for the PlayingCard class

--- src/test/java/edu/ntnu/idi/idatt2003/oving5/PlayingCardTest.java
+++ src/test/java/edu/ntnu/idi/idatt2003/oving5/PlayingCardTest.java
@@ -3,17 +3,23 @@ package edu.ntnu.idi.idatt2003.oving5;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.BeforeEach;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class PlayingCardTest {
 
   private PlayingCard testCard;
   private PlayingCard testCard2;
+  private PlayingCard identicalCard;
 
   @BeforeEach
   void setUp() {
     testCard = new PlayingCard('H', 4);
     testCard2 = new PlayingCard('S', 1);
+    identicalCard = new PlayingCard('H', 4);
+
   }
 
   @Test
@@ -38,5 +44,21 @@ class PlayingCardTest {
   void testIllegalCard() {
     assertThrows(IllegalArgumentException.class, () -> new PlayingCard('G', 12));
     assertThrows(IllegalArgumentException.class, () -> new PlayingCard('C', 0));
+  }
+
+  @Test
+  void testIdenticalCards() {
+    assertTrue(testCard.equals(identicalCard));
+    assertEquals(testCard.getAsString(), identicalCard.getAsString());
+    assertEquals(testCard.getSuit(), identicalCard.getSuit());
+    assertEquals(testCard.getFace(), identicalCard.getFace());
+  }
+
+  @Test
+  void testNonIdenticalCards() {
+    assertFalse(testCard.equals(testCard2));
+    assertNotEquals(testCard.getAsString(), testCard2.getAsString());
+    assertNotEquals(testCard.getSuit(), testCard2.getSuit());
+    assertNotEquals(testCard.getFace(), testCard2.getFace());
   }
 }
