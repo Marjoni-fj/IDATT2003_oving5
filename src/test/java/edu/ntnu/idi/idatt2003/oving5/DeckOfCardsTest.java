feafc4ae1265518cfd74258ef7c03fa11f7d2a10
Fredrik Marjoni <marjonifredrik@gmail.com>
2026-02-22 11:47:33 +0100

    test[DeckOfCards]: Add both positive and negative JUnit tests for the DeckOfCards class

--- src/test/java/edu/ntnu/idi/idatt2003/oving5/DeckOfCardsTest.java
+++ src/test/java/edu/ntnu/idi/idatt2003/oving5/DeckOfCardsTest.java
@@ -1,11 +1,9 @@
 package edu.ntnu.idi.idatt2003.oving5;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class DeckOfCardsTest {
   private DeckOfCards deck;
@@ -13,5 +11,28 @@ public class DeckOfCardsTest {
   @BeforeEach
   void setUp() {
     deck = new DeckOfCards();
+  }
+
+  @Test
+  void testDeckSize() {
+    assertEquals(52, deck.dealHand(52).size());
+  }
+
+  @Test
+  void testDealHandSize() {
+    assertEquals(5, deck.dealHand(5).size());
+  }
+
+  @Test
+  void testDealHandUniqueness() {
+    var hand = deck.dealHand(5);
+    assertEquals(5, hand.size());
+    assertEquals(5, hand.stream().distinct().count());
+  }
+
+  @Test
+  void testDealHandInvalidSize() {
+    assertThrows(IllegalArgumentException.class, () -> deck.dealHand(-1));
+    assertThrows(IllegalArgumentException.class, () -> deck.dealHand(53));
   }
 }
