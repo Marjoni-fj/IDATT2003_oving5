a5c9e07e5648a1537bca31286cade4f76abaa4d2
Fredrik Marjoni <marjonifredrik@gmail.com>
2026-02-25 13:11:06 +0100

    feat: Add first parts of GUI elements and test run with JavaFX

--- src/main/java/edu/ntnu/idi/idatt2003/oving5/App.java
+++ src/main/java/edu/ntnu/idi/idatt2003/oving5/App.java
@@ -1,13 +1,30 @@
 package edu.ntnu.idi.idatt2003.oving5;
 
+import javafx.application.Application;
+import javafx.stage.Stage;
+
 /**
  * Hello world!
  *
  */
-public class App 
+public class App extends Application
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main( String[] args ) {
+      launch(args);
+      System.out.println( "Hello World!" );
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+    primaryStage.setTitle("My first stage with scene");
+    primaryStage.show();
+    }
+    @Override
+    public void init() throws Exception {
+    super.init();
+    }
+    @Override
+    public void stop() throws Exception {
+    super.stop();
     }
 }
