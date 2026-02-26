7b371e57c4091cb18a6b65692436869641781e38
Fredrik Marjoni <marjonifredrik@gmail.com>
2026-02-26 20:23:59 +0100

    feat[App]: Add basic JavaFX nodes in the Poker application

--- src/main/java/edu/ntnu/idi/idatt2003/oving5/App.java
+++ src/main/java/edu/ntnu/idi/idatt2003/oving5/App.java
@@ -1,6 +1,10 @@
 package edu.ntnu.idi.idatt2003.oving5;
 
 import javafx.application.Application;
+import javafx.scene.Cursor;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 /**
@@ -16,7 +20,16 @@ public class App extends Application
 
     @Override
     public void start(Stage primaryStage) {
-    primaryStage.setTitle("My first stage with scene");
+    primaryStage.setTitle("Poker");
+    BorderPane rootNode = new BorderPane();
+
+    Button dealHand = new Button("Deal Hand");
+    rootNode.setRight(dealHand);
+
+    dealHand.setOnMouseEntered(e -> dealHand.setCursor(Cursor.HAND));
+    dealHand.setOnMouseExited(e -> dealHand.setCursor(Cursor.DEFAULT));
+    
+    primaryStage.setScene(new Scene(rootNode, 750, 750));
     primaryStage.show();
     }
     @Override
