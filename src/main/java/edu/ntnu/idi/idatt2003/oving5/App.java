b642a77da8269b73b8b1b2c6ee625b573c825563
Fredrik Marjoni <marjonifredrik@gmail.com>
2026-02-28 12:24:45 +0100

    feat: add possability to see random cards being delt

--- src/main/java/edu/ntnu/idi/idatt2003/oving5/App.java
+++ src/main/java/edu/ntnu/idi/idatt2003/oving5/App.java
@@ -1,10 +1,16 @@
 package edu.ntnu.idi.idatt2003.oving5;
 
+import java.util.List;
+
 import javafx.application.Application;
+import javafx.geometry.Insets;
 import javafx.scene.Cursor;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 /**
@@ -23,12 +29,32 @@ public class App extends Application
     primaryStage.setTitle("Poker");
     BorderPane rootNode = new BorderPane();
 
+    HBox cardDisplay = new HBox();
+    cardDisplay.setSpacing(10);
+    cardDisplay.setPadding(new Insets(20));
+    rootNode.setCenter(cardDisplay);
+
     Button dealHand = new Button("Deal Hand");
     rootNode.setRight(dealHand);
 
     dealHand.setOnMouseEntered(e -> dealHand.setCursor(Cursor.HAND));
     dealHand.setOnMouseExited(e -> dealHand.setCursor(Cursor.DEFAULT));
     
+    // When button is pressed, show the cards
+    dealHand.setOnAction(e -> {
+    cardDisplay.getChildren().clear();
+
+    DeckOfCards deck = new DeckOfCards();
+    List<PlayingCard> dealtCards = deck.dealHand(5);
+    HandOfCards hand = new HandOfCards(dealtCards);
+    List<PlayingCard> cards = hand.getHandOfCards();
+
+    for (PlayingCard card : cards) {
+        Text cardText = new Text(card.getAsString());
+        cardText.setFont(Font.font(30));
+        cardDisplay.getChildren().add(cardText);
+    }
+});
     primaryStage.setScene(new Scene(rootNode, 750, 750));
     primaryStage.show();
     }
